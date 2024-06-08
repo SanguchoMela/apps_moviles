8d001314976019bc7061d69d4215ae496838ea0e
SanguchoMela <melany01-03@hotmail.com>
2024-06-07 23:00:08 -0500

    feat: create files storage with firebase

---
+++ archivosFire/src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyCqElSPXGOCWCxNJBzK8J7bZZhX959dkY8',
+    authDomain: 'moviles-21d05.firebaseapp.com',
+    projectId: 'moviles-21d05',
+    storageBucket: 'moviles-21d05.appspot.com',
+    messagingSenderId: '912970924691',
+    appId: '1:912970924691:web:81daac96f0b61d0eb3c366',
+  },
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
