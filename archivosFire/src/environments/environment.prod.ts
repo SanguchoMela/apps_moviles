e603cecf652039c1ed73b6b99bde5be846519e5e
SanguchoMela <melany01-03@hotmail.com>
2024-06-07 23:25:21 -0500

    feat: delete personal credentials

--- archivosFire/src/environments/environment.prod.ts
+++ archivosFire/src/environments/environment.prod.ts
@@ -1,11 +1,3 @@
 export const environment = {
-  production: true,
-  firebaseConfig: {
-    apiKey: 'AIzaSyCqElSPXGOCWCxNJBzK8J7bZZhX959dkY8',
-    authDomain: 'moviles-21d05.firebaseapp.com',
-    projectId: 'moviles-21d05',
-    storageBucket: 'moviles-21d05.appspot.com',
-    messagingSenderId: '912970924691',
-    appId: '1:912970924691:web:81daac96f0b61d0eb3c366',
-  },
+  production: true
 };
--- archivosFire/src/environments/environment.ts
+++ archivosFire/src/environments/environment.ts
@@ -3,15 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  firebaseConfig: {
-    apiKey: 'AIzaSyCqElSPXGOCWCxNJBzK8J7bZZhX959dkY8',
-    authDomain: 'moviles-21d05.firebaseapp.com',
-    projectId: 'moviles-21d05',
-    storageBucket: 'moviles-21d05.appspot.com',
-    messagingSenderId: '912970924691',
-    appId: '1:912970924691:web:81daac96f0b61d0eb3c366',
-  },
+  production: false
 };
 
 /*
